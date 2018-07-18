0a6ac3e76bd5c58d09bd6ed700ff637be460bb4a
Christy Waickem <waickem@gmail.com>
2018-07-19 02:33:10 +0300

    [dynamic] getQueryParam function

--- js/core.js
+++ js/core.js
@@ -105,6 +105,23 @@ const HeaderControls = {
     }
 };
 
+const URL = {
+    getQueryParam: (sParam) => {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    }
+};
+
 
 // EXECUTE
 
